d2793c3a877a263dcf0480ed4e0c55c257485683
NsikanPatrick <nsikanpatrick69@gmail.com>
2022-06-17 18:16:59 +0200

    First commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
